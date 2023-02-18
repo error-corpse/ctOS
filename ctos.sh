33f261734a25ef5ce7b14f060b59b59a21069772
error-corpse <123547302+error-corpse@users.noreply.github.com>
2023-02-18 11:29:25 +0530

    Add files via upload

---
+++ ctos.sh
@@ -0,0 +1,27 @@
+clear
+
+figlet DedSeC
+figlet ctOS W3B
+echo
+echo
+echo Checking...........
+echo |
+echo /
+echo —
+echo \
+echo |
+echo
+echo confrigring.......
+echo
+echo
+
+apt install git -y
+apt install python -y
+apt install python3 -y
+apt install python2 -y
+
+echo only 4 testing./
+echo completed...
+echo
+echo
+figlet U   R   LIVE !!
